71b302274fe529ac1050e5d75404f8612c171252
Paulo-gsilva <guigui041201@gmail.com>
2022-03-31 14:06:39 -0300

    criando arquivos rpg

---
+++ RPG/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace dotnet_poo{
+	
+	class Program{
+		
+		static void Main(String[] args){
+			string nome = "Arus";
+			int level = 12;
+			string tipoHeroi = "Guerreiro";
+			
+			Console.Write(nome + " " + level + " " + tipoHeroi);
+		}
+	}
+}
